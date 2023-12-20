5c04eb4a8eb3c2494ba29005c02a1e51a34ff1a7
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-12-20 17:40:25 -0300

    feat: add next auth login

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import { Link } from "@nextui-org/react";
+import { Link, Spinner } from "@nextui-org/react";
 
 import Logo from "../../../public/logoWhite.png";
 import { Button } from "@nextui-org/react";
@@ -7,7 +8,22 @@ import { MdOutlineAdminPanelSettings } from "react-icons/md";
 import { TbDoorExit } from "react-icons/tb";
 import { RiAdminLine } from "react-icons/ri";
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export function Header() {
+  const { status, data } = useSession();
+
+  console.log(data?.user.name);
+  console.log(status);
+
+  async function handleLogin() {
+    await signIn();
+  }
+
+  async function handleLogout() {
+    await signOut();
+  }
+
   return (
     <header className="w-full mx-auto flex items-center px-2 py-4 h-20 drop-shadow-xl bg-[#333333]">
       <nav className="w-full max-w-5xl mx-auto flex items-center justify-around md:justify-between px-2 gap-6">
@@ -22,38 +38,50 @@ export function Header() {
         </Link>
 
         <div className="flex items-center justify-center gap-2">
-          <Link href="/">
-            <Button
-              endContent={<MdOutlineAdminPanelSettings size={20} />}
-              color="primary"
-              variant="shadow"
-              className="font-medium"
-            >
-              Entrar
-            </Button>
-          </Link>
+          {status === "loading" ? (
+            <Spinner size="md" color="primary" />
+          ) : (
+            <>
+              {status === "authenticated" ? (
+                <>
+                  <Link href="/dashboard">
+                    <Button
+                      endContent={<RiAdminLine size={20} />}
+                      color="primary"
+                      variant="shadow"
+                      className="font-medium"
+                    >
+                      Painel
+                    </Button>
+                  </Link>
 
-          <Link href="/dashboard">
-            <Button
-              endContent={<RiAdminLine size={20} />}
-              color="primary"
-              variant="shadow"
-              className="font-medium"
-            >
-              Painel
-            </Button>
-          </Link>
-
-          <Link href="/">
-            <Button
-              endContent={<TbDoorExit size={20} />}
-              color="danger"
-              variant="shadow"
-              className="font-medium"
-            >
-              Sair
-            </Button>
-          </Link>
+                  <Link href="/">
+                    <Button
+                      endContent={<TbDoorExit size={20} />}
+                      color="danger"
+                      variant="shadow"
+                      className="font-medium"
+                      onClick={handleLogout}
+                    >
+                      Sair
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <Link href="/">
+                  <Button
+                    endContent={<MdOutlineAdminPanelSettings size={20} />}
+                    color="primary"
+                    variant="shadow"
+                    className="font-medium"
+                    onClick={handleLogin}
+                  >
+                    Entrar
+                  </Button>
+                </Link>
+              )}
+            </>
+          )}
         </div>
       </nav>
     </header>
