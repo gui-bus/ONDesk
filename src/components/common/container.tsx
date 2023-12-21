5ab625e28dd698b94f68a53ddb09b7dc95284b00
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-12-20 21:37:53 -0300

    style: change container padding from p-5 to px-5

--- src/components/common/container.tsx
+++ src/components/common/container.tsx
@@ -1,4 +1,4 @@
 import { ReactNode } from "react";
 export function Container({ children }: { children: ReactNode }) {
-  return <div className=" w-full max-w-7xl mx-auto p-5">{children}</div>;
+  return <div className=" w-full max-w-7xl mx-auto px-5">{children}</div>;
 }
